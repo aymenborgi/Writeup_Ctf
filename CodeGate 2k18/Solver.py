3b544b1882bf756623b3e9314e0464ea42d4dc73
aymenborgi <aymenborgi007@gmail.com>
2018-02-04 11:40:37 +0100

    Update Solver.py

--- CodeGate 2k18/Solver.py
+++ CodeGate 2k18/Solver.py
@@ -10,3 +10,16 @@ path_group = proj.factory.path_group(threads=8)
 path_group.explore(find=find, avoid=avoid)
 
 print path_group.found[0].state.posix.dumps(0)
+
+'''
+┌─[root@parrot]─[~/Desktop/CTF/codegate]
+└──╼ #python Solver.py 
+WARNING | 2018-02-04 11:35:57,987 | angr.analyses.disassembly_utils | Your verison of capstone does not support MIPS instruction groups.
+WARNING | 2018-02-04 11:35:58,174 | angr.factory | factory.path_group() is deprecated! Please use factory.simgr() instead.
+What_You_Wanna_Be?:)_lc_laJ���
+                                �*J*�
+                                      J
+
+JJ
+   �*
+'''
